e9fc2e482745a269c1eeb8b2f8f7218e870da66f
by@park <vullro@gmail.com>
2020-09-21 17:44:21 +0900

    Create 19.staticMethod.js

---
+++ Nodejs/19.staticMethod.js
@@ -0,0 +1,32 @@
+class Human {
+	constructor(){
+		this._name = 'park'
+		this._age = 35
+	}
+
+	getName(){
+		return this._name
+	}
+	setName(name){
+		this._name = name
+	}
+	getAge(){
+		return this._age
+	}
+	setAge(age){
+		this._age = age
+	}
+	getInfo(){
+		return `I am ${this._name} and ${this._age} years old`
+	}
+
+}
+
+class Employee extends Human {
+	static fn(){
+		return 'hi'
+	}
+}
+
+console.log(Employee.fn())
+
