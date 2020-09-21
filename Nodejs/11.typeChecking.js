323a5993d19f69d2aaf44409c82499367b8f54e3
by@park <vullro@gmail.com>
2020-09-21 17:36:01 +0900

    Create 11.typeChecking.js

---
+++ Nodejs/11.typeChecking.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const string = '가나다라'
+const number = 111
+const array = ['1','2']
+const obj = {name : 'hi'}
+
+console.log('object string : ', typeof string)
+console.log('object number : ', typeof number)
+console.log('object array : ', typeof array)
+console.log('object type : ', typeof obj)
